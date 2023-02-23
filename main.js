f5d726a0463a72f1a6958723d7c6241215bb2e09
Kazani1823 <125144619+Kazani1823@users.noreply.github.com>
2023-02-23 16:24:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,44 @@ const swiper = new Swiper(".swiper", {
 });
 
 
+
+
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+
+// SHOW MENU
+menuBtn.addEventListener('click', () => {
+  menu.style.display = 'block';
+  menuBtn.style.display = 'none';
+  closeBtn.style.display = 'block';
+})
+
+// HIDE MENU
+closeBtn.addEventListener('click', () => {
+  menu.style.display = 'none';
+  menuBtn.style.display = 'block';
+  closeBtn.style.display = 'none';
+})
+
+
+const navItems = menu.querySelectorAll('li');
+
+const changeActiveItem = () => {
+  navItems.forEach(item => {
+    const link = item.querySelector('a');
+    link.classList.remove('active');
+  })
+}
+
+navItems.forEach(item => {
+  const link = item.querySelector('a');
+  link.addEventListener('click', () => {
+    changeActiveItem()
+    link.classList.add('active');
+  })
+})
+
 // SHOW MORE CONTENT //
 const readMoreBtn = document.querySelector(".read-more");
 const reamMoreContent = document.querySelector(".read-more-content");
